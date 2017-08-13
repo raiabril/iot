4730f58c47f18a0a12fc0685ced40042b6a420b3
Rai <rai@ingmechs.com>
2017-08-13 19:07:59 +0200

    Finished gasolineras

--- gasolineras.py
+++ gasolineras.py
@@ -11,15 +11,19 @@ import datetime
 
 PATH = '/Volumes/Macintosh HD/_Drive/_data'
 os.chdir(PATH)
-name = 'gasolineras_'+datetime.datetime.now().date().__str__()+'.json'
+name = 'gasolineras_'+datetime.datetime.now().date().__str__()+'_'+datetime.datetime.now().hour.__str__().zfill(2)+'-'+datetime.datetime.now().minute.__str__().zfill(2)+'.json'
 url = 'https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/'
 
 import requests
 import json
 
-#r = requests.get(url)
+r = requests.get(url)
+json_data = r.json()
 
-with open(name, 'w') as outfile:
-    json.dump(r.json(), outfile)
+json_string = json.dumps(json_data['ListaEESSPrecio'], ensure_ascii=False)
+
+with open(name, 'w', encoding='utf-8') as outfile:
+#    json.dump(json_data,outfile,ensure_ascii=False)
+    outfile.write(json_string)
     
 outfile.close()
