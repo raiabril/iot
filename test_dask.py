bc629aa182029c3d07efd40ef8458039d4653b64
raiabril <rai@ingmechs.com>
2017-10-02 18:16:49 +0200

    Added changes to collector

---
+++ test_dask.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 11 18:25:10 2017
+
+@author: root
+"""
+
+import os
+import datetime
+import dask.dataframe as dd
+
+PATH = '/media/pi/RAI/logs/temp'
+os.chdir(PATH)
