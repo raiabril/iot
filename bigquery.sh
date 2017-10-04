cd6dc90370da9e0fed470b1f718f65239a43eb1e
raiabril <rai@ingmechs.com>
2017-10-04 12:38:09 +0200

    Added comment

--- bigquery.sh
+++ bigquery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PATH="/home/pi/Downloads/google-cloud-sdk/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games"
-
+# Comment
 bq load --skip_leading_rows=1 cpb100-173514:gas_stations.Spain $(ls /media/pi/RAI/logs/gasolineras/*.csv | tail -1)
---
+++ loadmysql.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DB_USER='root';
+DB_PASSWD='J4v5f7o3';
+
+DB_NAME='mydb';
+TABLE='gas';
+
+INPUT_FILE="/media/pi/RAI/logs/gasolineras/gasolineras_2017-09-20_21-06.csv";
+
+SQL="USE $DB_NAME; LOAD LOCAL DATA INFILE '$INPUT_FILE' INTO TABLE `$TABLE` COLUMNS TERMINATED BY ',' LINES TERMINATED BY '\n' CHARACTER SET utf8;"
+mysql --user=$DB_USER --password=$DB_PASSWD --local-infile --default_character_set utf8 $DB_NAME --execute="$SQL"
