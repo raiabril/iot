abeaa15bd6a38a87badf7a3828a92f6152db5087
Rai <rai@ingmechs.com>
2017-10-11 16:57:32 +0200

    Loader mysql

---
+++ upload_mysql.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for f in *;
+do echo $f;
+mysql -e "load data local infile '"$f"' into table customs fields TERMINATED BY ',' LINES TERMINATED BY '\n'"  -u pi --password=J4v5f7o3 --local-infile;
+done
